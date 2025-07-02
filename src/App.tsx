268286247bb9253693a86ab2dcb57b97364f3a60
Diego Riquelme <diego.riquelme2016@gmail.com>
2025-07-02 01:33:40 -0400

    refactor: simplify authentication handling and update routing structure in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
-import React, { useState } from 'react';
-import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route, useNavigate, Navigate } from 'react-router-dom';
 import { Auth0Provider, withAuthenticationRequired } from '@auth0/auth0-react';
+import supabase from './lib/supabase/Supabase'; // Asegúrate de tener tu cliente de supabase configurado
 
-const ProtectedRoute = ({ component, ...args }) => {
-  const Component = withAuthenticationRequired(component, args);
-  return <Component />;
-};
 
-const Auth0ProviderWithRedirectCallback = ({ children, ...props }) => {
-  const navigate = useNavigate();
-  const onRedirectCallback = (appState) => {
-    navigate((appState && appState.returnTo) || window.location.pathname);
-  };
-  return (
-    <Auth0Provider onRedirectCallback={onRedirectCallback} {...props}>
-      {children}
-    </Auth0Provider>
-  );
+const ProtectedRoute = ({ children }) => {
+  const { session } = UserAuth(); // Asegúrate de que UserAuth esté correctamente importado desde tu contexto
+
+  if (session) {
+    return <>{children}</>;
+  } else {
+    return <Navigate to="/login" replace />;
+  }
 };
 
 // Layouts
@@ -32,32 +27,29 @@ import Ventas from './pages/Ventas';
 import Promociones from './pages/Promociones';
 import Reportes from './pages/Reportes';
 import GestionUsuarios from './pages/Usuarios';
+import { AuthContextProvider, UserAuth } from './context/AuthContext';
 
 function App() {
+  const {} = UserAuth(); // Asegúrate de que UserAuth esté correctamente importado desde tu contexto
+
   return (
-    <BrowserRouter>
-      <Auth0ProviderWithRedirectCallback
-          domain={import.meta.env.VITE_AUTH0_DOMAIN_NAME}
-          clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
-          authorizationParams={{
-            redirect_uri: window.location.origin,
-          }}
-        >
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<ProtectedRoute component={Layout} />}>
-          <Route index element={<ProtectedRoute component={Dashboard} />} />
-          <Route path="productos" element={<ProtectedRoute component={Productos} />} />
-          <Route path="clientes" element={<ProtectedRoute component={Clientes} />} />
-          <Route path="ventas" element={<ProtectedRoute component={Ventas} />} />
-          <Route path="promociones" element={<ProtectedRoute component={Promociones} />} />
-          <Route path="reportes" element={<ProtectedRoute component={Reportes} />} />
-          <Route path="usuarios" element={<ProtectedRoute component={GestionUsuarios} />} />
-          <Route path="*" element={<ProtectedRoute component={NotFound} />} />
-        </Route>
-      </Routes>
-      </Auth0ProviderWithRedirectCallback>
-    </BrowserRouter>
+      <BrowserRouter>
+          <AuthContextProvider>
+              <Routes>
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/" element={<ProtectedRoute><Layout /></ProtectedRoute>}>
+                      <Route index element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+                      <Route path="productos" element={<ProtectedRoute><Productos /></ProtectedRoute>}/>
+                      <Route path="clientes" element={<ProtectedRoute><Clientes /></ProtectedRoute>} />
+                      <Route path="ventas" element={<ProtectedRoute><Ventas /></ProtectedRoute>} />
+                      <Route path="promociones" element={<ProtectedRoute><Promociones /></ProtectedRoute>} />
+                      <Route path="reportes" element={<ProtectedRoute><Reportes /></ProtectedRoute>} />
+                      <Route path="usuarios" element={<ProtectedRoute><GestionUsuarios /></ProtectedRoute>} />
+                      <Route path="*" element={<ProtectedRoute><NotFound /></ProtectedRoute>} />
+                  </Route>
+              </Routes>
+          </AuthContextProvider>
+      </BrowserRouter>
   );
 }
 
