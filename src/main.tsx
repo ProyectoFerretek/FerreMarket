1dd61fb1a7b37717de14bbeafff602eeb6bdb00a
Diego Riquelme <diego.riquelme2016@gmail.com>
2025-07-02 01:33:49 -0400

    refactor: remove Auth0Provider from main.tsx and simplify rendering of App component

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Auth0Provider } from "@auth0/auth0-react";
 
 import App from "./App.tsx";
 import "./index.css";
@@ -8,13 +7,6 @@ import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <Auth0Provider
-            domain={import.meta.env.VITE_AUTH0_DOMAIN_NAME}
-            clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
-            authorizationParams={{
-                redirect_uri: window.location.origin,
-            }}>
-            <App />
-        </Auth0Provider>
+        <App />
     </StrictMode>
 );
